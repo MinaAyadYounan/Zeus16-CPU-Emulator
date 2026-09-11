ac3fb65993126af7979e02bdb3b63fa8314416ed
Mina Ayad <145565844+MinaAyadYounan@users.noreply.github.com>
2026-09-11 20:32:13 +0300

    Update Instruction.h

--- Zeus16_Team3/Instruction.h
+++ Zeus16_Team3/Instruction.h
@@ -22,7 +22,6 @@ struct Instruction {
     bool mem_write;   // Does this instruction write to Data Memory (Sw)?
     bool is_branch;   // Is this a BEQ instruction?
     bool is_jump;     // Is this a JAL instruction?
-    bool unknown_isa = false;
 };
 
 #endif // INSTRUCTION_H
